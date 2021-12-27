3706b2ba4c0813d2d25fb5133f07690dbce9c351
Mahmoud Atwa <m.atwa@hotmail.com>
2021-12-27 02:15:59 +0200

    Edit package.json

--- udagram/udagram-api/src/aws.ts
+++ udagram/udagram-api/src/aws.ts
@@ -1,11 +1,6 @@
 import AWS = require("aws-sdk");
 import { config } from "./config/config";
 
-// This is done to set 
-// Configure AWS
-const credentials = new AWS.SharedIniFileCredentials({ profile: config.aws_profile || "default" });
-AWS.config.credentials = credentials;
-
 export const s3 = new AWS.S3({
   signatureVersion: "v4",
   region: config.aws_region,
